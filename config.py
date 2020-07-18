14011d8a62a6a296281e766536e8d49c39fc7b45
groovestation31785 <angroover@gmail.com>
2020-07-18 16:46:15 -0700

    Allow GIF as one of the file types

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     SECRET_KEY = 'todo-change-this'
     IMAGE_UPLOADS = './static/img/uploads'
     IMAGE_DOWNLOADS = './static/img/downloads'
-    ALLOWED_IMAGE_EXTENSIONS = ["JPEG", "JPG", "PNG"]
+    ALLOWED_IMAGE_EXTENSIONS = ["JPEG", "JPG", "PNG", "GIF"]
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 465
     MAIL_USE_SSL = True
