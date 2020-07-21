63170876b2612f7196eaee468b6bc6b33faf9df8
groovestation31785 <angroover@gmail.com>
2020-07-20 21:17:38 -0700

    Skip iteration if measure data is empty

--- notes.py
+++ notes.py
@@ -70,7 +70,9 @@ def split_long_measures(arr, ref_array):
 def set_notes_values(notes_list):
     note_inputs = []
     for n in notes_list:
-        if len(n) <= 3:
+        if len(n) == 0:
+            continue
+        elif len(n) <= 4:
             note_inputs.append(pad_or_truncate(n,5))
         elif len(n) <= 6:
             uniq = unique(n)
@@ -82,13 +84,13 @@ def set_notes_values(notes_list):
 
 
 def write_inputs_to_csv(prepared_input, frame_no):
-    # fields = ['note1', 'note2', 'note3', 'not4', 'note5', 'key']
+    fields = ['note1', 'note2', 'note3', 'not4', 'note5', 'key']
     rows = prepared_input
     filename = f"chord_data/{frame_no}.csv"
 
     with open(filename, 'w') as csvfile:
         csvwriter = csv.writer(csvfile)
-        # csvwriter.writerow(fields)
+        csvwriter.writerow(fields)
         csvwriter.writerows(rows)
 
 
